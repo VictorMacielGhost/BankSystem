1688c34c47dea14571029d75b4f918d769277fb1
Victor <victormacieldesouzaofc@gmail.com>
2022-06-19 11:31:49 -0300

    Final Version

---
+++ BankSystem/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace BankSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User User = new User("null", 1, 0);
+            Console.WriteLine("Type The account Number: ");
+            int numberAcount = int.Parse(Console.ReadLine());
+            Console.WriteLine("Type The User name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("The account will have initial balance? (y/n)");
+            string action = Console.ReadLine();
+            if (action.Equals("n"))
+            {
+                User User1 = new User(userName, numberAcount);
+                User = User1;
+            }
+            else if(action.Equals("y"))
+            {
+                Console.WriteLine("Type The How Much The User will have of initial balance: ");
+                double balance = double.Parse(Console.ReadLine());
+                User User1 = new User(userName, numberAcount, balance);
+                User = User1;
+            }
+            else Console.WriteLine("Error! Only can be used y or N !");
+            User.ShowUserStats();
+            Console.WriteLine("Insert The Value Of the Deposit: ");
+            double deposit = double.Parse(Console.ReadLine());
+            User.MakeDeposit(deposit);
+            Console.WriteLine("Insert the value of the WithDrawal: ");
+            double withdrawal = double.Parse(Console.ReadLine());
+            User.MakeWithdrawal(withdrawal);
+        }
+    }
+}
---
+++ BankSystem/User.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankSystem
+{
+    class User
+    {
+        private string _userName;
+        private double _balance;
+        public int AccountNumber { get; private set; }
+
+        public User(string username, int accountnumber, double balance = 0.0)
+        {
+            if (SetUserName(username) == 0) Console.WriteLine("Error! User Name Might be More than 3 characters!");
+            if (SetUSerAccountNumber(accountnumber) == 0) Console.WriteLine("Error! Account Number Might Be More than 0!");
+            SetUserBalance(balance);
+        }
+
+        public string GetUserName()
+        {
+            return this._userName;
+        }
+
+        public int SetUserName(string name)
+        {
+            if (name.Length < 3) return 0;
+            else
+            {
+                name.Trim();
+                this._userName = name;
+                return 1;
+            }
+        }
+
+        public double GetUserBalance()
+        {
+            return this._balance;
+        }
+
+        public int SetUserBalance(double balance)
+        {
+            if (balance == 0) return 0;
+            else
+            {
+                this._balance += balance;
+                return 1;
+            }
+        }
+
+        private int SetUSerAccountNumber(int accountnumber)
+        {
+            if (accountnumber < 1) return 0;
+            else
+            {
+                this.AccountNumber = accountnumber;
+                return 1;
+            }
+        }
+
+        public int MakeDeposit(double desposit)
+        {
+            if (desposit < 1) return 0;
+            else
+            {
+                int result = this.SetUserBalance(desposit);
+                this.ShowUserStats();
+                return result;
+            }
+        }
+
+        public int MakeWithdrawal(double withdrawal)
+        {
+            if (withdrawal < 0) return 0;
+            else
+            {
+                withdrawal += 5;
+                int result = SetUserBalance(-withdrawal);
+                this.ShowUserStats();
+                return result;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Account Number: " + this.AccountNumber +
+                " User Name: " + this.GetUserName() +
+                " Balance: " + this.GetUserBalance() + " R$";
+        }
+
+        public void ShowUserStats()
+        {
+            Console.WriteLine(ToString());
+        }
+
+    }
+}
